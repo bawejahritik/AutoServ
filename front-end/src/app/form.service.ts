a18efd97a899ba7450e48b0f38d95c0de5945e36
raghavsoni114 <dsrv114@gmail.com>
2022-03-21 19:32:34 -0400

    Added GET request using Tracking ID

--- front-end/src/app/form.service.ts
+++ front-end/src/app/form.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +8,20 @@ export class FormService {
   url = 'http://localhost:8080/stars';
 
   constructor( private http: HttpClient ) { }
+
   saveValues(data:any) {
     console.log(data)
     let headers = new HttpHeaders({'Content-Type' : 'application/json'});
     let options = {headers: headers};
     return this.http.post<any>(this.url, JSON.stringify(data), options)
+  }
+
+  getValues() {
+    const httpParams = new HttpParams ({
+      fromObject: {
+        trackingID: '123456'
+      }
+    })
+    return this.http.get('http://localhost:8080/getClient', {params: httpParams});
   }
 }
--- front-end/src/app/track/track.component.ts
+++ front-end/src/app/track/track.component.ts
@@ -1,13 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormService } from '../form.service';
 
 @Component({
   selector: 'app-track',
   templateUrl: './track.component.html',
   styleUrls: ['./track.component.css']
 })
-export class TrackComponent implements OnInit {
+export class TrackComponent {
 
-  constructor() { }
+  constructor(private form: FormService) {}
+
+  getTrack() {
+    this.form.getValues().subscribe(res => {
+      console.log(res);
+    });
+  }
 
   ngOnInit(): void {
   }
