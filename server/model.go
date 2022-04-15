1946410c64d801aeb46751172cfe8e7f99243ca3
bawejahritik <bawejahritik@gmail.com>
2022-04-14 23:56:33 -0400

    Send status through GET response
    close #112

--- server/model.go
+++ server/model.go
@@ -17,6 +17,7 @@ type ClientDetails struct {
 	TrackingID         string `json:"trackingID"`
 	FirstName          string `json:"firstName"`
 	LastName           string `json:"lastName"`
+	Status             string `json:"status"`
 	CreatedAt          time.Time
 	UpdatedAt          time.Time
 }
