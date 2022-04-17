a447b34fedc39c565b2c4cbaea26868f215e0356
bawejahritik <bawejahritik@gmail.com>
2022-04-17 17:08:26 -0400

    Add backend unit test

--- server/main_test.go
+++ server/main_test.go
@@ -1,22 +1,36 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
-
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
 )
 
-const (
-	dbURI = "sqlite"
-)
+var a App
 
 func TestMain(m *testing.M) {
-	_, err := gorm.Open(sqlite.Open(dbURI), &gorm.Config{})
-	if err != nil {
-		panic("failed to connect database")
+	a.Initialize("sqlite", "testing.db")
+
+	if err := a.DB.Migrator().HasTable("client_details"); err != true {
+		log.Fatal(err)
 	}
 
-	os.Exit(m.Run())
+	code := m.Run()
+
+	a.DB.Migrator().DropTable("client_details")
+
+	os.Exit(code)
+
+}
+
+func TestGetNonExistentTrackingID(t *testing.T) {
+	req, _ := http.NewRequest("GET", "http://localhost:8080/getClient?trackingID=123456", nil)
+	w := httptest.NewRecorder()
+	a.r.ServeHTTP(w, req)
+
+	if want, got := http.StatusOK, w.Code; want == got {
+		t.Fatalf("expected a %d, instead got: %d", want, got)
+	}
 }
